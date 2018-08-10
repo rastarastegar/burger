47dfd22fd68b0795541cccef6f0a75b6219e5461
Reza R <rastegar8@gmail.com>
2018-08-09 21:43:56 -0500

    files initiated

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Angus Beef');
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Fish Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Grilled Chicken', true);
+INSERT INTO burgers (name) VALUES ('Double Pattie');
