deacf4832659c8c9bbd4b88681ff73aca260a4b0
Reza R <rastegar8@gmail.com>
2018-08-11 14:01:00 -0500

    heroku deployment set up

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (name) VALUES ('Angus Beef');
-INSERT INTO burgers (name) VALUES ('Cheese Burger');
-INSERT INTO burgers (name, devoured) VALUES ('Fish Burger', true);
-INSERT INTO burgers (name, devoured) VALUES ('Veggie Burger', true);
-INSERT INTO burgers (name, devoured) VALUES ('Grilled Chicken', true);
-INSERT INTO burgers (name) VALUES ('Double Pattie');
+INSERT INTO burgers (burger_name) VALUES ('Angus Beef');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fish Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grilled Chicken', true);
+INSERT INTO burgers (burger_name) VALUES ('Double Pattie');
